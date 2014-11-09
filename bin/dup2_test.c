02ad0afc92d008644e6bed44ab685ecfc52dacc3
Andrew Williams <williams.andrew@gmail.com>
2014-11-09 17:49:01 -0600

    Modify dup2_test program for new syscall functionality
    Modify the dup2_test program to also test `dup2` functionality if our
    destination file descriptor has not been opened.

--- bin/dup2_test.c
+++ bin/dup2_test.c
@@ -1,9 +1,11 @@
 /*
- * Tests the functionality of the `dup2` system call. With no arguments we open
- * two files and write out text. In this case we expect the file 'new_dup2' to
- * contain the string 'bar\n'. With one or more arguments we open two files,
- * perform a `dup2` system call and expect string 'bar\n' to expect in
- * 'old_dup2'
+ * Tests the functionality of the `dup2` system call. The first argument is the
+ * original file the we are duplicating. If a second argument is passed in we
+ * open this file, write a string to it, then duplicate it. This tests that our
+ * kernel is properly closing and duplicating the file. If the second argument
+ * is not passed in, we simply duplicate the original file to descriptor 10. In
+ * both cases the original file should have the string "foobar\n". If not, then
+ * something went wrong
  */
 
 #include "types.h"
@@ -13,33 +15,37 @@
 
 int
 main(int argc, char *argv[]) {
-  int oldfd, newfd;
+    int origfd, newfd;
 
-  // Dont care if the files dont exist already
-  unlink("old_dup2");
-  unlink("new_dup2");
-
-  if ((oldfd = open("old_dup2", O_CREATE|O_RDWR)) < 0) {
-      printf(1, "Cannot open 'old_dup2'\n");
+    if (argc < 2) {
+      printf(2, "%s: Not enough arguments\n", argv[0]);
+      printf(2, "Usage: origfile [newfile]\n");
       exit();
-  }
-  if ((newfd = open("new_dup2", O_CREATE|O_RDWR)) < 0) {
-      printf(1, "Cannot open 'new_dup2'\n");
-      exit();
-  }
-
-  write(oldfd, "foo", 3);
-  write(newfd, "err", 3);
-
-  if (argc > 1) {
-    printf(0, "Attempting dup2\n");
-
-    if (dup2(oldfd, newfd) < 0) {
-      printf(1, "dup2 error: \n");
     }
-  }
 
-  write(newfd, "bar\n", 4);
+    unlink(argv[1]);
 
-  exit();
+    if ((origfd = open(argv[1], O_CREATE|O_RDWR)) < 0) {
+      printf(2, "Cannot open '%s'\n", argv[2]);
+      exit();
+    }
+    if (argc > 2) {
+      unlink(argv[3]);
+      if ((newfd = open(argv[2], O_CREATE|O_RDWR)) < 0) {
+          printf(2, "Cannot open '%s'\n", argv[3]);
+          exit();
+      }
+      write(newfd, "ignored\n", 8);
+    } else {
+      newfd = 10;
+    }
+
+    write(origfd, "foo", 3);
+    if (dup2(origfd, newfd) < 0) {
+        printf(2, "dup2 error\n");
+    }
+
+    write(newfd, "bar\n", 4);
+
+    exit();
 }
